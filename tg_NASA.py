217be7eae779175c4212933b9e3e9fb7effd4818
dunkel13 <fedom9@gmail.com>
2019-06-09 22:47:49 -0500

    Update tg_NASA.py

--- tg_NASA.py
+++ tg_NASA.py
@@ -21,16 +21,13 @@ rcParams['figure.figsize'] = 15, 6
 
 #Usamos Pandas para manejar las bases de Datos
 tg_NASA=pd.read_csv('C:/Users/FAMILIA MORENO/Documents/FMpy/tgNASA.txt', delimiter=r"\s+", header=None, names=('Year', 'No_Smoothing', 'Lowess(5)'))
-tg_NASA.head(10)
 tg_NASA.tail(10)
-
-ind = pd.date_range(start='1880', end='2019', freq='Y')
-print(ind)
-###########################
-### TRANSFORMACIÓN BOX COX
-###########################
 tg_NASA.min()
-bc_tg_NASA = pd.DataFrame(tg_NASA['No_Smoothing'] + 0.73) 
+ind = pd.date_range(start='1880', end='2019', freq='Y')
+########################################################
+###
+########################################################
+bc_tg_NASA = pd.DataFrame(tg_NASA['No_Smoothing'] + 0.73)
 # MIN (No_Smoothing) = -0.48, EN [R]: minimum data value <= 0 so -min+0.25 added to all values, LUEGO -(-0.48)+0.25=0.73
 print(bc_tg_NASA)
 bc_tg_NASA = bc_tg_NASA.set_index(ind)
@@ -40,27 +37,22 @@ plt.plot(bc_ts_tgN)
 plt.show()
 import scipy as sp
 import scipy.stats ####En ocasiones puede funcionar la línea 46 sin ésta línea.
-sp.stats.boxcox(bc_tg_NASA['No_Smoothing'],alpha=0.05) 
+sp.stats.boxcox(bc_tg_NASA['No_Smoothing'],alpha=0.05)  #Como el cero está en el intervalo,se aplica logaritmo
 print(sp.stats.boxcox(bc_tg_NASA['No_Smoothing'],alpha=0.05))
-
+########################################################
 ###
-
-
-
-tg_NASA = tg_NASA.set_index(ind)
-ts_tgN = pd.Series(tg_NASA['No_Smoothing'],index=ind)
-print(ts_tgN) 
+########################################################
+df_tgN = pd.DataFrame(tg_NASA['No_Smoothing'])
+df_tgN = df_tgN.set_index(ind)
+ts_tgN = pd.Series(df_tgN['No_Smoothing'],index=ind)
 plt.plot(ts_tgN)
 plt.show()
-#import statsmodels.api as smapi
-#smapi.graphics.tsa.month_plot(tsAirP)
 acf(ts_tgN,nlags=50,unbiased=False)
 Grafico_acf_tgN = plot_acf(ts_tgN,lags=50,unbiased=False)
-import scipy as sp
-import scipy.stats ####En ocasiones puede funcionar la línea 46 sin ésta línea.
-sp.stats.boxcox(tg_NASA['No_Smoothing'],alpha=0.05) 
-print(sp.stats.boxcox(ts_tgN['No_Smoothing'],alpha=0.05))
-np.minimum(tg_NASA['No_Smoothing'])
-print(tg_NASA['No_Smoothing'])
-
-print(ts_tgN)
+########################################################
+###
+########################################################
+lts_tgN=np.log(ts_tgN)
+plt.plot(lts_tgN)
+d1lts_tgN=lts_tgN.diff(periods=1)[1:] #diferencia ordinaria
+plt.plot(d1lts_tgN)
