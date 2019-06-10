93e4436140fbaa9fd3ba3a3088b099ab83c00594
dunkel13 <fedom9@gmail.com>
2019-06-09 21:35:16 -0500

    Create tg_NASA.py

---
+++ tg_NASA.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  9 20:35:45 2019
+
+@author: FM
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.tsa.stattools import acf
+from statsmodels.tsa.stattools import pacf
+from statsmodels.graphics.tsaplots import plot_pacf
+from statsmodels.tsa.arima_model import ARIMA
+#from sm.tsa.statespace import SARIMAX
+from matplotlib.pylab import rcParams
+rcParams['figure.figsize'] = 15, 6
+
+#Usamos Pandas para manejar las bases de Datos
+tg_NASA=pd.read_csv('C:/Users/FAMILIA MORENO/Documents/FMpy/tgNASA.txt', delimiter=r"\s+", header=None, names=('Year', 'No_Smoothing', 'Lowess(5)'))
+tg_NASA.head(10)
+tg_NASA.tail(10)
+
+ind = pd.date_range(start='1880', end='2019', freq='Y')
+print(ind)
+tg_NASA = tg_NASA.set_index(ind)
+ts_tgN = pd.Series(tg_NASA['No_Smoothing'],index=ind)
+print(ts_tgN) 
+plt.plot(ts_tgN)
+plt.show()
+#import statsmodels.api as smapi
+#smapi.graphics.tsa.month_plot(tsAirP)
+acf(ts_tgN,nlags=50,unbiased=False)
+Grafico_acf_tgN = plot_acf(ts_tgN,lags=50,unbiased=False)
+import scipy as sp
+import scipy.stats ####En ocasiones puede funcionar la línea 46 sin ésta línea.
+sp.stats.boxcox(tg_NASA['No_Smoothing'],alpha=0.05) 
+print(sp.stats.boxcox(ts_tgN['No_Smoothing'],alpha=0.05))
+np.minimum(tg_NASA['No_Smoothing'])
+print(tg_NASA['No_Smoothing'])
+
+print(ts_tgN)
